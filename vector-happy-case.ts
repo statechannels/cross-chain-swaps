bef6c5442d85536b5fac7f9afaac199073892821
geoknee <georgeknee@googlemail.com>
2021-03-18 18:31:41 +0000

    convert vector happy case over to erc20 tokens

--- vector-happy-case.ts
+++ vector-happy-case.ts
@@ -7,6 +7,7 @@ import { CoreChannelState, CoreTransferState } from "@connext/vector-types";
 import { hashChannelCommitment, ChannelSigner } from "@connext/vector-utils";
 import { solidityKeccak256 } from "ethers/lib/utils";
 import { SWAP_AMOUNT } from "./constants";
+import { TestContractArtifacts } from "@statechannels/nitro-protocol";
 
 // See https://github.com/connext/vector/blob/main/modules/protocol/src/testing/integration/happy.spec.ts
 // Will it be easier to use vector class instances (wallets)? Or try and go state-by-state as we did with nitro?
@@ -75,27 +76,6 @@ const rightChain = new ethers.providers.JsonRpcProvider(
 );
 
 (async function () {
-  const executor: Actor = {
-    signingWallet: executorWallet,
-    log: (s: string) => console.log(chalk.keyword("orangered")("> " + s)),
-    gasSpent: 0,
-    getLeftBalance: async () =>
-      await leftChain.getBalance(executorWallet.address),
-    getRightBalance: async () =>
-      await rightChain.getBalance(executorWallet.address),
-  };
-  const responder: Actor = {
-    signingWallet: responderWallet,
-    log: (s: string) => console.log(chalk.keyword("gray")("< " + s)),
-    gasSpent: 0,
-    getLeftBalance: async () =>
-      await leftChain.getBalance(responderWallet.address),
-    getRightBalance: async () =>
-      await rightChain.getBalance(responderWallet.address),
-  };
-
-  await logBalances(executor, responder);
-
   // SETUP CONTRACTS ON BOTH CHAINS
   // Deploy the contracts to chain, and then reconnect them to their respective signers
   // for the rest of the interactions
@@ -104,20 +84,44 @@ const rightChain = new ethers.providers.JsonRpcProvider(
     leftChannelFactory,
     leftHashLock,
     leftTransferRegistry,
+    leftToken,
   ] = await deployContractsToChain(leftChain);
   const [
     rightChannelMasterCopy,
     rightChannelFactory,
     rightHashLock,
     rightTransferRegistry,
+    rightToken,
   ] = await deployContractsToChain(rightChain);
+
+  const executor: Actor = {
+    signingWallet: executorWallet,
+    log: (s: string) => console.log(chalk.keyword("orangered")("> " + s)),
+    gasSpent: 0,
+    getLeftBalance: async () =>
+      await leftToken.balanceOf(executorWallet.address),
+    getRightBalance: async () =>
+      await rightToken.balanceOf(executorWallet.address),
+  };
+  const responder: Actor = {
+    signingWallet: responderWallet,
+    log: (s: string) => console.log(chalk.keyword("gray")("< " + s)),
+    gasSpent: 0,
+    getLeftBalance: async () =>
+      await leftToken.balanceOf(responderWallet.address),
+    getRightBalance: async () =>
+      await rightToken.balanceOf(responderWallet.address),
+  };
+
+  await logBalances(executor, responder);
 
   const leftCore = await fundChannel(
     leftChain,
     executor,
     responder,
     leftChannelFactory,
-    leftChannelMasterCopy
+    leftChannelMasterCopy,
+    leftToken
   );
 
   const leftConditionalTransfer: CoreTransferState = {
@@ -139,7 +143,8 @@ const rightChain = new ethers.providers.JsonRpcProvider(
     responder,
     executor,
     rightChannelFactory,
-    rightChannelMasterCopy
+    rightChannelMasterCopy,
+    rightToken
   );
 
   // given the longChannel is now funded and running
@@ -158,14 +163,16 @@ const rightChain = new ethers.providers.JsonRpcProvider(
     responder,
     leftChain,
     leftChannelFactory,
-    leftChannelMasterCopy
+    leftChannelMasterCopy,
+    leftToken
   );
   await defundChannel(
     rightCore.channelAddress,
     responder,
     executor,
     rightChain,
-    responder
+    responder,
+    rightToken
   );
 
   await logBalances(executor, responder);
@@ -212,11 +219,17 @@ async function deployContractsToChain(chain: ethers.providers.JsonRpcProvider) {
     deployer
   ).deploy();
 
+  const token = await ContractFactory.fromSolidity(
+    TestContractArtifacts.TokenArtifact,
+    deployer
+  ).deploy(await chain.getSigner(0).getAddress());
+
   return [
     channelMasterCopy,
     channelFactory,
     hashLock,
     transferRegistry,
+    token,
   ].map((contract) => contract.connect(chain.getSigner(0)));
 }
 /**
@@ -232,7 +245,8 @@ async function fundChannel(
   proposer: Actor,
   joiner: Actor,
   channelFactory: Contract,
-  channelMasterCopy: Contract
+  channelMasterCopy: Contract,
+  token?: Contract
 ) {
   const { chainId } = await chain.getNetwork();
   const channelAddress = ethers.utils.getCreate2Address(
@@ -253,7 +267,7 @@ async function fundChannel(
     // should have the to field filled out
     alice: joiner.signingWallet.address,
     bob: proposer.signingWallet.address,
-    assetIds: [ethers.constants.AddressZero],
+    assetIds: [token.address],
     balances: [{ amount: [SWAP_AMOUNT], to: [responderWallet.address] }],
     processedDepositsA: [],
     processedDepositsB: [],
@@ -262,12 +276,19 @@ async function fundChannel(
     merkleRoot: "",
   };
 
-  const { gasUsed } = await (
-    await chain.getSigner().sendTransaction({
-      to: channelAddress,
-      value: core.balances[0].amount[0],
-    })
-  ).wait(); // Note that we ignore who *actually* sent the transaction, but attribute it to the proposer here
+  let gasUsed;
+  if (token) {
+    ({ gasUsed } = await (
+      await token.transfer(channelAddress, core.balances[0].amount[0])
+    ).wait());
+  } else {
+    ({ gasUsed } = await (
+      await chain.getSigner().sendTransaction({
+        to: channelAddress,
+        value: core.balances[0].amount[0],
+      })
+    ).wait()); // Note that we ignore who *actually* sent the transaction, but attribute it to the proposer here
+  }
 
   proposer.gasSpent += Number(gasUsed);
   proposer.log(
@@ -280,7 +301,8 @@ async function createAndFundChannel(
   proposer: Actor,
   joiner: Actor,
   channelFactory: Contract,
-  channelMasterCopy: Contract
+  channelMasterCopy: Contract,
+  token?: Contract
 ) {
   const { chainId } = await chain.getNetwork();
   const channelAddress = ethers.utils.getCreate2Address(
@@ -315,7 +337,7 @@ async function createAndFundChannel(
     // should have the to field filled out
     alice: proposer.signingWallet.address,
     bob: joiner.signingWallet.address,
-    assetIds: [ethers.constants.AddressZero],
+    assetIds: [token ? token.address : ethers.constants.AddressZero],
     balances: [{ amount: [SWAP_AMOUNT], to: [responderWallet.address] }],
     processedDepositsA: [],
     processedDepositsB: [],
@@ -341,15 +363,25 @@ async function createAndFundChannel(
   // confused: it looks like Alice calls deposit AND does a regular transfer; Bob does just a transfer
   // https://github.com/connext/vector/blob/54f050202290769b0d672d362493b783610908dd/modules/protocol/src/testing/utils/channel.ts#L151-L186
   // Alice creates the multisig and deposits in one tx
+
+  const { gasUsed: gasUsedForAllowance } = await (
+    await token.increaseAllowance(
+      channelFactory.address,
+      core.balances[0].amount[0]
+    )
+  ).wait();
+
+  proposer.gasSpent += Number(gasUsedForAllowance);
+  proposer.log(
+    `spent ${gasUsedForAllowance} gas increasing allownace for ChannelFactory`
+  );
+
   const { gasUsed } = await (
     await channelFactory.createChannelAndDepositAlice(
       proposer.signingWallet.address,
       joiner.signingWallet.address,
-      ethers.constants.AddressZero,
-      core.balances[0].amount[0],
-      {
-        value: core.balances[0].amount[0],
-      }
+      token ? token.address : ethers.constants.AddressZero,
+      core.balances[0].amount[0]
     )
   ).wait(); // Note that we ignore who *actually* sent the transaction, but attribute it to the executor here
   // ideally we check that the new contract deployed at the address we expect
@@ -368,7 +400,8 @@ async function defundChannel(
   proposer: Actor,
   joiner: Actor,
   chain: ethers.providers.JsonRpcProvider,
-  gasPayer: Actor
+  gasPayer: Actor,
+  token?: Contract
 ) {
   const { chainId } = await chain.getNetwork();
   const channel = await new Contract(
@@ -382,7 +415,7 @@ async function defundChannel(
     proposer.signingWallet.address,
     joiner.signingWallet.address,
     joiner.signingWallet.address,
-    ethers.constants.AddressZero,
+    token ? token.address : ethers.constants.AddressZero,
     SWAP_AMOUNT,
     "1"
   );
@@ -411,7 +444,8 @@ async function createAndDefundChannel(
   joiner: Actor,
   chain: ethers.providers.JsonRpcProvider,
   channelFactory: Contract,
-  channelMasterCopy: Contract
+  channelMasterCopy: Contract,
+  token?: Contract
 ) {
   const { chainId } = await chain.getNetwork();
 
@@ -438,7 +472,7 @@ async function createAndDefundChannel(
     proposer.signingWallet.address,
     joiner.signingWallet.address,
     joiner.signingWallet.address,
-    ethers.constants.AddressZero,
+    token ? token.address : ethers.constants.AddressZero,
     SWAP_AMOUNT,
     "1"
   );
@@ -467,12 +501,12 @@ async function logBalances(...actors: Actor[]) {
     actor.log(
       `I have ${(
         await actor.getLeftBalance()
-      ).toString()} wei on the left chain`
+      ).toString()} tokens on the left chain`
     );
     actor.log(
       `I have ${(
         await actor.getRightBalance()
-      ).toString()} wei on the right chain`
+      ).toString()} tokens on the right chain`
     );
   }
 }
